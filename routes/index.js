7320d4bdbd17568c588358f91237da6d82166f49
Randy Rebucas <rebucasrandy1986@gmail.com>
2019-06-25 20:58:57 +0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+var express = require('express');
+var router = express.Router();
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+  const apiKey = process.env.SHOPIFY_API_KEY;
+  const apiSecret = process.env.SHOPIFY_API_SECRET;
+
+  const filestackAPI = process.env.FILESTACK_API;
+  res.render('index', { 
+    title: 'Filestack Shopify API' , 
+    apiKey: apiKey, 
+    apiSecret: apiSecret,
+    filestackAPI: filestackAPI
+  });
+});
+
+module.exports = router;
