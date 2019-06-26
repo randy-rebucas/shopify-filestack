02e6273b9ca232dfc9f63bfe57a192237b6e717f
Randy rebucas <rebucasrandy1986@gmail.com>
2019-06-26 14:46:47 +0800

    update product routes

--- routes/products.js
+++ routes/products.js
@@ -5,8 +5,8 @@ const shopifyAPI = require('shopify-node-api');
 
 var Shopify = new shopifyAPI({
     shop: 'benmessina', // MYSHOP.myshopify.com
-    shopify_api_key: '944e0ac35098ec7629d83483a02d9e77', // Your API key
-    access_token: 'e1e718dc744474987097a760f9d154ea' // Your API password
+    shopify_api_key: '664220ffb95667516895f191a11b9820', // Your API key
+    access_token: 'e9e59dadee510d8bcec1837712c3786b' // Your API password
 });
 
 /* GET home page. */
@@ -21,72 +21,67 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
     var post_data = {
-      "product": {
-        "title": req.body.format,
-        "body_html": "<strong>Good snowboard!</strong>",
-        "vendor": "benmessina", //use active user
-        "product_type": "Custom Frame",
-        "metafields_global_title_tag": "Brand new " + req.body.format,
-        "metafields_global_description_tag": "Brand new " + req.body.format,
-        "tags": "custom-frames, sizes, borders", 
-        "published": true,
-        "created_at": new Date().toISOString(),
-        "fulfillment_service": "manual",
-        "requires_shipping": false,
-        "images": [
-          {
-              "src": req.body.image // frame aspects
-          }
-        ],
-        "options":[
-          {
-             "position":1,
-             "name":"Size"
-          },
-          {
-             "position":2,
-             "name":"Frame"
-          },
-          {
-             "position":3,
-             "name":"Border"
-          }
-        ],
-        "variants": [
-          {
-            //"image_id": req.body.image,          //filestack image id
-            "option1": req.body.size,         // string
-            "option2": req.body.frame,        // string
-            "option3": req.body.border,       // string
-            "price": "10.00",                 // generate price depend on selection
-            "sku": "1234"                     // generate uniqe sku
-          }
-        ]
-      }
+        "product": {
+            "title": req.body.format,
+            "body_html": "<strong>Good snowboard!</strong>",
+            "vendor": "benmessina", //use active user
+            "product_type": "Custom Frame",
+            "metafields_global_title_tag": "Brand new " + req.body.format,
+            "metafields_global_description_tag": "Brand new " + req.body.format,
+            "tags": "custom-frames, sizes, borders",
+            "published": true,
+            "created_at": new Date().toISOString(),
+            "fulfillment_service": "manual",
+            "requires_shipping": false,
+            "images": [{
+                "src": req.body.image // frame aspects
+            }],
+            "options": [{
+                    "position": 1,
+                    "name": "Size"
+                },
+                {
+                    "position": 2,
+                    "name": "Frame"
+                },
+                {
+                    "position": 3,
+                    "name": "Border"
+                }
+            ],
+            "variants": [{
+                //"image_id": req.body.image,          //filestack image id
+                "option1": req.body.size, // string
+                "option2": req.body.frame, // string
+                "option3": req.body.border, // string
+                "price": "10.00", // generate price depend on selection
+                "sku": "1234" // generate uniqe sku
+            }]
+        }
     }
-  	Shopify.post('/admin/products.json', post_data, function(err, data, headers){
-      //res.json(data);
-      res.status(200).redirect('/products');
-	  });
+    Shopify.post('/admin/products.json', post_data, function(err, data, headers) {
+        //res.json(data);
+        res.status(200).redirect('/products');
+    });
 });
 
 router.put('/', function(req, res, next) {
     var put_data = {
-    "product": {
-      "body_html": "<strong>Updated!</strong>"
+        "product": {
+            "body_html": "<strong>Updated!</strong>"
+        }
     }
-  }
-   
-  Shopify.put('/admin/products/1234567.json', put_data, function(err, data, headers){
-    console.log(data);
-  });
- 
+
+    Shopify.put('/admin/products/1234567.json', put_data, function(err, data, headers) {
+        console.log(data);
+    });
+
 });
 
 router.delete('/', function(req, res, next) {
-  Shopify.delete('/admin/products/1234567.json', function(err, data, headers){
-    console.log(data);
-  });
+    Shopify.delete('/admin/products/1234567.json', function(err, data, headers) {
+        console.log(data);
+    });
 });
 
 module.exports = router;
--- routes/shopify.js
+++ routes/shopify.js
@@ -10,7 +10,7 @@ const request = require('request-promise');
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
 const scopes = 'read_products';
-const forwardingAddress = 'https://4c9a5512.ngrok.io'; // Replace this with your HTTPS Forwarding address
+const forwardingAddress = 'https://0f151a3c.ngrok.io'; // Replace this with your HTTPS Forwarding address
 
 router.get('/', (req, res) => {
     const shop = req.query.shop;
@@ -18,85 +18,85 @@ router.get('/', (req, res) => {
         const state = nonce();
         const redirectUri = forwardingAddress + '/shopify/callback';
         const installUrl = 'https://' + shop +
-        '/admin/oauth/authorize?client_id=' + apiKey +
-        '&scope=' + scopes +
-        '&state=' + state +
-        '&redirect_uri=' + redirectUri;
-  
+            '/admin/oauth/authorize?client_id=' + apiKey +
+            '&scope=' + scopes +
+            '&state=' + state +
+            '&redirect_uri=' + redirectUri;
+
         res.cookie('state', state);
         res.redirect(installUrl);
     } else {
         return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
     }
 });
-  
+
 router.get('/callback', (req, res) => {
 
     const { shop, hmac, code, state } = req.query;
-  const stateCookie = cookie.parse(req.headers.cookie).state;
+    const stateCookie = cookie.parse(req.headers.cookie).state;
 
-  if (state !== stateCookie) {
-    return res.status(403).send('Request origin cannot be verified');
-  }
-
-  if (shop && hmac && code) {
-    // DONE: Validate request is from Shopify
-    const map = Object.assign({}, req.query);
-    delete map['signature'];
-    delete map['hmac'];
-    const message = querystring.stringify(map);
-    const providedHmac = Buffer.from(hmac, 'utf-8');
-    const generatedHash = Buffer.from(
-      crypto
-        .createHmac('sha256', apiSecret)
-        .update(message)
-        .digest('hex'),
-        'utf-8'
-      );
-    let hashEquals = false;
-
-    try {
-      hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
-    } catch (e) {
-      hashEquals = false;
-    };
-
-    if (!hashEquals) {
-      return res.status(400).send('HMAC validation failed');
+    if (state !== stateCookie) {
+        return res.status(403).send('Request origin cannot be verified');
     }
 
-    // DONE: Exchange temporary code for a permanent access token
-    const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
-    const accessTokenPayload = {
-      client_id: apiKey,
-      client_secret: apiSecret,
-      code,
-    };
+    if (shop && hmac && code) {
+        // DONE: Validate request is from Shopify
+        const map = Object.assign({}, req.query);
+        delete map['signature'];
+        delete map['hmac'];
+        const message = querystring.stringify(map);
+        const providedHmac = Buffer.from(hmac, 'utf-8');
+        const generatedHash = Buffer.from(
+            crypto
+            .createHmac('sha256', apiSecret)
+            .update(message)
+            .digest('hex'),
+            'utf-8'
+        );
+        let hashEquals = false;
 
-    request.post(accessTokenRequestUrl, { json: accessTokenPayload })
-    .then((accessTokenResponse) => {
-      const accessToken = accessTokenResponse.access_token;
-      // DONE: Use access token to make API call to 'shop' endpoint
-      const shopRequestUrl = 'https://' + shop + '/admin/api/2019-04/shop.json';
-      const shopRequestHeaders = {
-        'X-Shopify-Access-Token': accessToken,
-      };
+        try {
+            hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+        } catch (e) {
+            hashEquals = false;
+        };
 
-      request.get(shopRequestUrl, { headers: shopRequestHeaders })
-      .then((shopResponse) => {
-        //var response = shopResponse;
-        //console.log(response.shop);
-        //res.status(200).end(response);
-        res.status(200).redirect('/');
-        //res.render('shopify', { title: 'Shopify', data:  shopResponse});
-      })
-      .catch((error) => {
-        res.status(error.statusCode).send(error.error.error_description);
-      });
-    })
-    .catch((error) => {
-      res.status(error.statusCode).send(error.error.error_description);
-    });
+        if (!hashEquals) {
+            return res.status(400).send('HMAC validation failed');
+        }
+
+        // DONE: Exchange temporary code for a permanent access token
+        const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+        const accessTokenPayload = {
+            client_id: apiKey,
+            client_secret: apiSecret,
+            code,
+        };
+
+        request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+            .then((accessTokenResponse) => {
+                const accessToken = accessTokenResponse.access_token;
+                // DONE: Use access token to make API call to 'shop' endpoint
+                const shopRequestUrl = 'https://' + shop + '/admin/api/2019-04/shop.json';
+                const shopRequestHeaders = {
+                    'X-Shopify-Access-Token': accessToken,
+                };
+
+                request.get(shopRequestUrl, { headers: shopRequestHeaders })
+                    .then((shopResponse) => {
+                        //var response = shopResponse;
+                        //console.log(response.shop);
+                        //res.status(200).end(response);
+                        res.status(200).redirect('/');
+                        //res.render('shopify', { title: 'Shopify', data:  shopResponse});
+                    })
+                    .catch((error) => {
+                        res.status(error.statusCode).send(error.error.error_description);
+                    });
+            })
+            .catch((error) => {
+                res.status(error.statusCode).send(error.error.error_description);
+            });
 
     } else {
         res.status(400).send('Required parameters missing');
