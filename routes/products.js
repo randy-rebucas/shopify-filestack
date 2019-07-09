09dbc244be989456c6d2af901b91cc1720f9010a
Randy Rebucas <rebucasrandy1986@gmail.com>
2019-07-09 10:21:39 +0800

    update product create page

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
+
 const shopifyConnection = require('../middleware/shopify-api');
 
 const ProductController = require('../controllers/products');
 /* GET home page. */
 
 router.get('/create', function(req, res, next) {
-    res.render('products-create', { title: 'Create Products' });
+    res.render('products-create', { 
+        title: 'Create Products', 
+        urlHost: req.get('host'), 
+        urlProtocol: req.protocol 
+    });
 });
 
 router.get('/', shopifyConnection, ProductController.getProducts );
