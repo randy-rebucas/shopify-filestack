de263cf5ef3026e48dbc0fbf40a15070cbb9d469
Randy Rebucas <rebucasrandy1986@gmail.com>
2019-07-11 21:44:38 +0800

    update 1/11 9:44pm

--- controllers/metafields.js
+++ controllers/metafields.js
@@ -1,7 +1,5 @@
 exports.getShop = (req, res, next) => {
-
     req.shopifyToken.get('/admin/api/2019-04/metafields.json', function(err, data, headers) {
-        //res.json(data.metafields);
         res.render('index', {
             title: 'Filestack Shopify API',
             metafields: data.metafields
@@ -10,18 +8,30 @@ exports.getShop = (req, res, next) => {
 }
 
 exports.createShop = (req, res, next) => {
-
-    //res.json(req.body);
     var val = req.body;
-    var metafileds_data = {
-        "metafield": {
-            "namespace": "fs_api",
-            "key": "filestackAPI",
-            "value": val.filestackAPI,
-            "value_type": "string"
+    if(val.filestackId) {
+        var metafileds_data = {
+            "metafield": {
+                "id": val.filestackId,
+                "value": val.filestackAPI,
+                "value_type": "string"
+            }
         }
+        var metafieldIdJson = val.filestackId + '.json';
+        req.shopifyToken.put('/admin/api/2019-04/metafields/' + metafieldIdJson, metafileds_data, function(err, data, headers) {
+            res.redirect('/');
+        });
+    } else {
+        var metafileds_data = {
+            "metafield": {
+                "namespace": "fs_api",
+                "key": "filestackAPI",
+                "value": val.filestackAPI,
+                "value_type": "string"
+            }
+        }
+        req.shopifyToken.post('/admin/api/2019-04/metafields.json', metafileds_data, function(err, data, headers) {
+            res.redirect('/');
+        });
     }
-    req.shopifyToken.post('/admin/api/2019-04/metafields.json', metafileds_data, function(err, data, headers) {
-        res.json(data);
-    });
 }
